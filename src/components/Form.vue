f54b4d07b4833a9c96ab034c31069addc021b6c8
callum-whatley <cwhatley@live.ca>
2025-02-14 00:21:21 -0700

    Added new font; Tweaked setup forms layout to better align with intended theme;

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,75 +1,106 @@
 <template>
-  <div>
-    <form ref="myForm">
-      <div class="form-group row my-2">
-        <label for="time" class="col-8">How Long For Each Mushodoro?</label>
-        <div class="col-4">
-          <input
-            type="number"
-            class="form-control text-center w-75"
-            id="time"
-            min="1"
-            value="1"
-          />
-        </div>
-      </div>
-      <div class="form-group row my-2">
-        <label for="break" class="col-8">How Long For Each Break?</label>
-        <div class="col-4">
-          <input
-            type="number"
-            class="form-control text-center w-75"
-            id="break"
-            min="1"
-            value="1"
-          />
-        </div>
-      </div>
-      <div class="form-group row my-2">
-        <label for="rounds" class="col-8">How Many Rounds?</label>
-        <div class="col-4">
-          <input
-            type="number"
-            class="form-control text-center w-75"
-            id="rounds"
-            min="1"
-            value="1"
-          />
-        </div>
-      </div>
-      <button type="button" @click="addMushodoroTimer" class="btn btn-primary">
-        Submit
-      </button>
-    </form>
-  </div>
+	<div>
+		<form ref="myForm">
+			<div class="form-group row">
+				<label for="time" class="col-9" style="align-content: center">
+					How Long Per Mushodoro?
+				</label>
+				<div class="col-3">
+					<input
+						type="number"
+						class="form-control text-center"
+						id="time"
+						min="1"
+						value="25"
+					/>
+				</div>
+			</div>
+			<div class="form-group row my-2">
+				<label for="break" class="col-9" style="align-content: center">
+					How Long Per Break?
+				</label>
+				<div class="col-3">
+					<input
+						type="number"
+						class="form-control text-center"
+						id="break"
+						min="1"
+						value="5"
+					/>
+				</div>
+			</div>
+			<div class="form-group row">
+				<label for="rounds" class="col-9" style="align-content: center">
+					How Many Rounds?
+				</label>
+				<div class="col-3">
+					<input
+						type="number"
+						class="form-control text-center"
+						id="rounds"
+						min="1"
+						value="1"
+					/>
+				</div>
+			</div>
+			<button
+				type="button"
+				@click="addMushodoroTimer"
+				class="btn btn-primary mt-4"
+			>
+				Submit
+			</button>
+		</form>
+	</div>
 </template>
 
 <script>
-export default {
-  name: "Form",
-  methods: {
-    addMushodoroTimer() {
-      let formData = {
-        minutes: this.$refs.myForm[0].value,
-        break: this.$refs.myForm[1].value,
-        rounds: this.$refs.myForm[2].value,
-      };
-      this.$store.dispatch("addMushodoro", formData);
-      this.$store.dispatch("backgroundHandler");
-      this.$router.push("/timer");
-    },
-  },
-};
+	export default {
+		name: 'Form',
+		methods: {
+			addMushodoroTimer() {
+				let formData = {
+					minutes: this.$refs.myForm[0].value,
+					break: this.$refs.myForm[1].value,
+					rounds: this.$refs.myForm[2].value,
+				};
+				this.$store.dispatch('addMushodoro', formData);
+				this.$store.dispatch('backgroundHandler');
+				this.$router.push('/timer');
+			},
+		},
+	};
 </script>
 
 <style>
-button {
-  background-color: var(--main-colour) !important;
-  border-color: var(--main-colour) !important;
-  margin: 1rem;
-}
+	button {
+		background-color: var(--main-colour) !important;
+		border-color: var(--main-colour) !important;
+		color: var(--bg-colour) !important;
+		margin: 1rem;
+	}
 
-label {
-  color: white;
-}
+	label {
+		color: var(--bg-colour);
+		font-weight: 900;
+		font-size: 1.18rem;
+	}
+
+	input {
+		background-color: var(--main-colour) !important;
+		border: 0.15rem solid var(--bg-colour) !important;
+		color: var(--bg-colour) !important;
+	}
+
+	/* Chrome, Safari, Edge, Opera */
+	input[type='number']::-webkit-inner-spin-button,
+	input[type='number']::-webkit-outer-spin-button {
+		-webkit-appearance: none;
+		margin: 0;
+	}
+
+	/* Firefox */
+	input[type='number'] {
+		-moz-appearance: textfield;
+	}
 </style>
